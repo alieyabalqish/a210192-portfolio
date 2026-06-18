1e4f9668ac0ccbacd21a81722945b70f743a926e
alieyabalqish <alieyaazman74@gmail.com>
2026-06-19 01:47:35 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A210192_AlieyaBalqish_DrNiza_Project2"
+include(":app")
